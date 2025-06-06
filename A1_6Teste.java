7137507a7d445e122e6e7806add07e5820a6957d
Eduardo Cardoso <102430093+Hyriuky@users.noreply.github.com>
2025-06-06 00:28:31 +0000

    Aula 05/06

---
+++ A1_6Teste.java
@@ -0,0 +1,10 @@
+public class A1_6Teste {
+    public static void main (String[] args) {
+
+        // String (não pe um tipo primitivo
+        String texto = "Exemplo de texto";
+
+        // Exibindo os valores
+        System.out.println("String: " + texto);
+    }
+}
